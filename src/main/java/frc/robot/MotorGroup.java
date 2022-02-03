6817a656d81cde1b5c4eabd6bd40c841a9abcbbc
team3704 <61070767+team3704@users.noreply.github.com>
2022-02-03 15:53:04 -0800

    Initial Setup
    yes

---
+++ src/main/java/frc/robot/MotorGroup.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+public class MotorGroup {
+    /**
+     * @param thing hello
+     */
+    public MotorGroup(TalonSRX frontL, TalonSRX backL, TalonSRX frontR, TalonSRX backR) {
+
+    }
+    
+}
