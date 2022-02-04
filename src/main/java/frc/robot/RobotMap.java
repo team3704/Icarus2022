f027f0a24286a6e4411725f570dcc0b7faa72d91
team3704 <61070767+team3704@users.noreply.github.com>
2022-02-03 17:36:44 -0800

    test

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,9 +1,7 @@
 package frc.robot;
 
 public class RobotMap {
-    public static int MOTOR_PORT_LEFT = 1;
-    final int i = 3;
-    int t = thing.i;
+    public static fina
 }
 class thing {
     static int i = RobotMap.MOTOR_PORT_LEFT;
