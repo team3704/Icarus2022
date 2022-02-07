572d729fad17eb6cbe71c91e1631646f089e8f98
KrustyKrabbyBoi <whitneyjason60@gmail.com>
2022-02-07 10:00:14 -0800

    vision stuff

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class IO {
+    // define commonly used objects here
     public static final XboxController c_xbox = new XboxController(RobotMap.XBOX_PORT);
     public static final Joystick c_stick_left = new Joystick(RobotMap.JOYSTICKL_PORT);
     public static final Joystick c_stick_right = new Joystick(RobotMap.JOYSTICKR_PORT);
---
+++ src/main/java/frc/robot/Vision.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import edu.wpi.first.networktables.*;
+
+public class Vision {
+    private static NetworkTableInstance nti   = NetworkTableInstance.getDefault();
+    private static NetworkTable nt            = nti.getTable("limelight");
+    private static NetworkTableEntry
+        angle_h  = nt.getEntry("tx"), // target offset angle horozontal
+        angle_v  = nt.getEntry("ty"), // target offset angle vertical
+        area     = nt.getEntry("ta"), // target area
+        skew     = nt.getEntry("ts"); // target skew
+    // api: https://docs.limelightvision.io/en/latest/networktables_api.html
+    // docs: https://docs.limelightvision.io/en/latest/
+    // estimating distance: https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+    // downloads: https://limelightvision.io/pages/downloads/ (finder tool is essential!)
+    // default adress: http://10.37.4.28:5801/ (use to manage the limelight)
+    // TODO: tune the limelight using the adress above
+
+    public static void update() {
+
+    }
+}
