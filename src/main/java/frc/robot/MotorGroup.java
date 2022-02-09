fa1e687c736d90ab0e8eeb622f7e8861df1cbeba
team3704 <61070767+team3704@users.noreply.github.com>
2022-02-09 15:15:30 -0800

    revert

--- src/main/java/frc/robot/MotorGroup.java
+++ src/main/java/frc/robot/MotorGroup.java
@@ -4,15 +4,11 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.BaseTalon;
 
 public class MotorGroup {
-    BaseTalon[] motors;
+    public BaseTalon[] motors;
     public MotorGroup(BaseTalon[] motors) {
         this.motors = motors;
     }
     public void set(double powerPercent) {
         for(BaseTalon motor : motors) motor.set(ControlMode.PercentOutput, powerPercent);
-    }
-    // invert moters before getting passed into group (or keep a reference to them to invert later)
-    public void invert(int motorIndex, Boolean inverted) {
-        motors[motorIndex].setInverted(true);
     }
 }
