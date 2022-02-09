05800b81135d4a3176ba27fe993f8983ce60c9a0
KrustyKrabbyBoi <whitneyjason60@gmail.com>
2022-02-09 15:08:28 -0800

    magazineload is a command! reformat!!

--- src/main/java/frc/robot/commands/MagazineLoad.java
+++ src/main/java/frc/robot/commands/MagazineLoad.java
@@ -1,26 +1,17 @@
 package frc.robot.commands;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.IO;
 import frc.robot.MotorGroup;
 
 public class MagazineLoad extends CommandBase
 {
-    static TalonSRX[] motors = new TalonSRX[]
-    {
-      new TalonSRX(5), new TalonSRX(2)
-    };
-    static MotorGroup IntakeMotors = new MotorGroup(motors);
-    public static void moveMotor() 
-    {
-      double leftT = IO.c_xbox.getLeftTriggerAxis();
-      IntakeMotors.set(leftT);
+    MotorGroup IntakeMotors;
+    public double input = 0;
+    @Override public void execute() {
+      IntakeMotors.set(input);
     }
-    public static void moveMotor(int index) 
-    {
-      double leftT = IO.c_xbox.getLeftTriggerAxis();
-      motors[index].set(ControlMode.PercentOutput, leftT);
+    public MagazineLoad() {
+      IntakeMotors = new MotorGroup(new TalonSRX[] {new TalonSRX(5), new TalonSRX(2)});
     }
 }
