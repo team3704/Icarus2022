ed269640a3cdad0b95de0cdb57f0515bd1d4fac2
KrustyKrabbyBoi <whitneyjason60@gmail.com>
2022-02-09 15:22:00 -0800

    mappings

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,9 @@ public final class RobotMap {
         MOTOR_PORT_FR = 5,
         MOTOR_PORT_BR = 1,
 
+        MOTOR_PORT_M0 = 5,
+        MOTOR_PORT_M1 = 2,
+
         JOYSTICKL_PORT = 0,
         JOYSTICKR_PORT = 1,
         XBOX_PORT = 2; // xbox controller object has an enum for button ids!
--- src/main/java/frc/robot/commands/MagazineLoad.java
+++ src/main/java/frc/robot/commands/MagazineLoad.java
@@ -3,6 +3,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.MotorGroup;
+import frc.robot.RobotMap;
 
 public class MagazineLoad extends CommandBase
 {
@@ -10,7 +11,10 @@ public class MagazineLoad extends CommandBase
     public double input = 0;
 
     public MagazineLoad() {
-      IntakeMotors = new MotorGroup(new TalonSRX[] {new TalonSRX(5), new TalonSRX(2)});
+      IntakeMotors = new MotorGroup(new TalonSRX[] {
+        new TalonSRX(RobotMap.MOTOR_PORT_M0),
+        new TalonSRX(RobotMap.MOTOR_PORT_M1)
+      });
     }
     @Override public void execute() {
       IntakeMotors.set(input);
