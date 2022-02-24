a9ea0f74f6583ea3a4791d60f782617adc732dda
KrustyKrabbyBoi <whitneyjason60@gmail.com>
2022-02-24 09:22:38 -0800

    ditto

--- src/main/java/frc/robot/subsystems/BallTrack.java
+++ src/main/java/frc/robot/subsystems/BallTrack.java
@@ -12,7 +12,7 @@ import frc.robot.Constants;
 /** Does everything to move the balls around the robot. */
 public class BallTrack extends SubsystemBase {
     PWMSparkMax m_sparky = new PWMSparkMax(Constants.PWM.m_arm); // arm
-    //TODO: opps again, the spark max MUST be on the CAN bus for all features :/
+    //TODO: oops again, the spark max MUST be on the CAN bus for all features :/
     WPI_TalonFX m_sl     = new WPI_TalonFX(Constants.CAN.m_shooter[0]);
 
     MotorControllerGroup mg_shooter = new MotorControllerGroup(m_sl);
