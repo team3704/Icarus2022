d197aa1ca1938754431c9a7be16bf92b403bb6c9
KrustyKrabbyBoi <whitneyjason60@gmail.com>
2022-02-24 08:24:14 -0800

    spork mox

--- src/main/java/frc/robot/subsystems/BallTrack.java
+++ src/main/java/frc/robot/subsystems/BallTrack.java
@@ -3,14 +3,13 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 /** Does everything to move the balls around the robot. */
 public class BallTrack extends SubsystemBase {
-    Spark       m_sparky = new Spark(Constants.PWM.m_arm); // arm
-    //TODO: AAAAA THE SPARK MAX CAN DO LIMITS!!!!!!!!!!! USE AN ENCODER TO KEEP AT POSITION!!!!!!!!! OOPS!!
+    PWMSparkMax m_sparky = new PWMSparkMax(Constants.PWM.m_arm); // arm
     WPI_TalonFX m_sl     = new WPI_TalonFX(Constants.CAN.m_shooter[0]);
 
     MotorControllerGroup mg_shooter = new MotorControllerGroup(m_sl);
